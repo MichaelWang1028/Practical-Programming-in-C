9c06966f3c6b52ac5402108fe95ad168aead4782
M <lmiguelvargasf@gmail.com>
2016-05-02 12:49:42 -0500

    Define stack_node struct

--- lecture07/stack_linked_list.c
+++ lecture07/stack_linked_list.c
@@ -1,1 +1,4 @@
+#include <stdlib.h>
 #include "stack_linked_list.h"
+
+stack_node * stack_buffer = NULL;
--- lecture07/stack_linked_list.h
+++ lecture07/stack_linked_list.h
@@ -1,6 +1,11 @@
 #ifndef STACK_LINKED_LIST_H
 #define STACK_LINKED_LIST_H
 
+typedef struct stack_node {
+  int element;
+  struct stack_node * next_node;
+} stack_node;
 
+extern stack_node * stack_buffer;
 
 #endif
