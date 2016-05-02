759d0d11d267ba48e398fac035f56c4708c393ef
M <lmiguelvargasf@gmail.com>
2016-05-02 14:39:54 -0500

    Create variables to implement a queue using an array

--- lecture07/queue_array.c
+++ lecture07/queue_array.c
@@ -1,1 +1,5 @@
 #include "queue_array.h"
+
+double queue_buffer[100];
+int i_front;
+int i_count;
--- lecture07/queue_array.h
+++ lecture07/queue_array.h
@@ -1,6 +1,8 @@
 #ifndef QUEUE_ARRAY_H
 #define QUEUE_ARRAY_H
 
-
+extern double queue_buffer[100];
+extern int i_front;
+extern int i_count;
 
 #endif
