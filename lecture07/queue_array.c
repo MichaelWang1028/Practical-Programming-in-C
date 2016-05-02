70d25d6688cd2dac446d4d62cf7403eb08e05a9c
M <lmiguelvargasf@gmail.com>
2016-05-02 14:55:01 -0500

    Implement enqueue and dequeue functions

--- lecture07/queue_array.c
+++ lecture07/queue_array.c
@@ -1,5 +1,22 @@
 #include "queue_array.h"
 
-double queue_buffer[100];
-int i_front;
-int i_count;
+int queue_buffer[100];
+int i_front = 0;
+int i_count = 0;
+
+void enqueue(double element)
+{
+  if (i_count < NUMBER_OF_ELEMENTS) {
+    queue_buffer[i_front + i_count] = element;
+    i_count++;
+  }
+}
+
+int dequeue()
+{
+  if (i_count > 0) {
+    i_count--;
+    return queue_buffer[i_front++];
+  }
+  return -1;
+}
--- lecture07/queue_array.h
+++ lecture07/queue_array.h
@@ -1,8 +1,13 @@
 #ifndef QUEUE_ARRAY_H
 #define QUEUE_ARRAY_H
 
-extern double queue_buffer[100];
+#define NUMBER_OF_ELEMENTS 100
+
+extern int queue_buffer[NUMBER_OF_ELEMENTS];
 extern int i_front;
 extern int i_count;
+
+void enqueue(double element);
+int dequeue();
 
 #endif
