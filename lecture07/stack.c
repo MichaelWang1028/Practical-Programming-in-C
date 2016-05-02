0c23d73785dcd614bf936f286f45ded609b8d017
M <lmiguelvargasf@gmail.com>
2016-05-02 12:15:12 -0500

    Implement stack as array

--- lecture07/stack.c
+++ lecture07/stack.c
@@ -1,1 +1,18 @@
 #include "stack.h"
+
+int stack_buffer[100];
+int i_top = 0;
+
+void push(int elem)
+{
+  stack_buffer[i_top++] = elem;
+}
+
+int pop()
+{
+  if (i_top > 0) {
+    return stack_buffer[--i_top];
+  } else {
+    return -1;
+  }
+}
--- lecture07/stack.h
+++ lecture07/stack.h
@@ -1,6 +1,10 @@
 #ifndef STACK_H
 #define STACK_H
 
+extern stack_buffer[100];
+extern int i_top;
 
+void push(int elem);
+int pop();
 
 #endif
