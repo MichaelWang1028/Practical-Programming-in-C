59a79e6341027927b6781ecf47f4e358e657541e
M <lmiguelvargasf@gmail.com>
2016-05-03 16:50:13 -0500

    Remove files to start pset from scratch

--- pset6a/prob1.c
+++ pset6a/prob1.c
@@ -1,14 +1,45 @@
 /*
  * prob1.c
  *
- *  Created on:
- *      Author:
+ *  Created on: 
+ *      Author: 
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "prob1.h"
+
+/* maximum length of input string (including newline character) */
+#define INPUT_MAX 2048
+
+/* enable (1) or disable (0) parentheses checking in parsing strings */
+/* leave disabled for part (a); enable for part (b) */
+#define PARSE_PARENS 0
+
+/* type of token */
+enum token_type {
+	OPERAND, /* number */
+	OPERATOR, /* operator: +, -, *, / */
+#if PARSE_PARENS
+	LPARENS, /* left parentheses ( */
+	RPARENS /* right parentheses ) */
+#endif
+};
+
+/* operator identifiers (opcodes) */
+enum op {
+	ADD, /* a+b */
+	SUBTRACT, /* a-b (binary) */ 
+	MULTIPLY, /* a*b */
+	DIVIDE, /* a/b */
+	NEGATE /* -a (unary) */
+};
+
+/* direction of evaluation (associativity) */
+enum assoc {
+	LEFT, /* left-to-right (+, binary -, *, /) */
+	RIGHT /* right-to-left (unary -) */
+};
 
 /* number of operands for each operator type */
 const unsigned int op_operands[] = {2, 2, 2, 2, 1};
@@ -19,11 +50,87 @@ const unsigned int op_precedences[] = {0, 0, 1, 1, 2};
 /* evaluation direction (associativity) for each precedence level */
 const enum assoc op_associativity[] = {LEFT, LEFT, RIGHT};
 
+/* contains value of token */
+union token_value {
+	double operand; /* numeric value for operand */
+	enum op op_code; /* opcode for operators */
+};
+
+/* data structure for token */
+typedef struct s_expr_token {
+	union token_value value; /* numeric value or opcode */
+	enum token_type type; /* type of token */
+
+	struct s_expr_token * linked_token; /* linked token in stack/queue */
+} * p_expr_token; /* p_expr_token is shorthand for "struct s_expr_token *" */
+
+/* data structure for queue */
+struct token_queue {
+	p_expr_token front; /* front of queue, where tokens are dequeued */
+	p_expr_token back; /* back of queue, where tokens are added */
+};
+
+/* queue functions - enqueue and dequeue */
+void enqueue(struct token_queue * pqueue, const p_expr_token ptoken);
+p_expr_token dequeue(struct token_queue * pqueue);
+
+/* stack functions - push and pop */
+void push(p_expr_token * ptop, const p_expr_token ptoken);
+p_expr_token pop(p_expr_token * ptop);
+
+/* creates a new token in dynamic memory (using malloc()) */
+p_expr_token new_token(const enum token_type type, const union token_value value);
+
+/* constructs a queue of tokens in infix order from a space-delimited string */
+struct token_queue expr_to_infix(char * str);
+
+/* creates a queue of tokens in postfix order from a queue of tokens in infix order */
+/* postcondition: returned queue contains all the tokens, and pqueue_infix should be 
+   empty */
+struct token_queue infix_to_postfix(struct token_queue * pqueue_infix);
+
+/* evalutes the postfix expression stored in the queue */
+/* postcondition: returned value is final answer, and pqueue_postfix should be empty */
+double evaluate_postfix(struct token_queue * pqueue_postfix);
+
+/* handles evaluation process (calls above functions) for expression string str */
+double evaluate(const char * str);
+
+int main(void) {
+	char input[INPUT_MAX];
+	double ans;
+	unsigned int len;
+
+	do {
+		printf("Enter an expression to evaluate: ");
+		fflush(stdout);
+		if (!fgets(input, INPUT_MAX, stdin))
+			abort(); /* failed to read stdin */
+		
+		len = strlen(input);
+		/* remove trailing newline character */
+		if (len > 0 && input[len-1] == '\n') {
+			input[len-1] = '\0';
+			--len;
+		}
+		if (len == 0) /* empty expression signals exit */
+			break;
+
+		/* call evaluation functions */
+		ans = evaluate(input);
+
+		/* write result to stdout */
+		printf("%s => %g\n", input, ans);
+	} while (1);
+
+	return 0;
+}
+
 /* enqueue (add) token to end of queue
    input: pqueue - pointer to queue
           ptoken - token pointer to add
    postcondition: token added to end of queue */
-void enqueue(token_queue * pqueue, const p_expr_token ptoken) {
+void enqueue(struct token_queue * pqueue, const p_expr_token ptoken) {
 	ptoken->linked_token = NULL;
 	if (pqueue->back)
 		pqueue->back->linked_token = ptoken;
@@ -36,7 +143,7 @@ void enqueue(token_queue * pqueue, const p_expr_token ptoken) {
    input: pointer to queue
    output: front token pointer (or NULL, if queue was empty)
    postcondition: token removed from queue */
-p_expr_token dequeue(token_queue * pqueue) {
+p_expr_token dequeue(struct token_queue * pqueue) {
 	p_expr_token ptoken = pqueue->front;
 	if (pqueue->front) {
 		pqueue->front = ptoken->linked_token;
@@ -56,7 +163,7 @@ void push(p_expr_token * ptop, const p_expr_token ptoken) {
 	*ptop = ptoken;
 }
 
-/* pop (remove) token from top of stack
+/* pop (remove) token from top of stack 
    input: pointer to top token pointer of stack
    output: top token pointer (or NULL, if stack was empty)
    postcondition: ptop points to next token in stack */
@@ -70,7 +177,7 @@ p_expr_token pop(p_expr_token * ptop) {
 }
 
 /* allocate new token on heap, with specified type and value; the token is initially
-   un-linked (field "linked_token" == NULL)
+   un-linked (field "linked_token" == NULL) 
    note: token must be freed using free() after use */
 p_expr_token new_token(const enum token_type type, const union token_value value) {
 	p_expr_token ptoken = (p_expr_token)malloc(sizeof(struct s_expr_token));
@@ -85,7 +192,7 @@ p_expr_token new_token(const enum token_type type, const union token_value value
 double evaluate(const char * str) {
 	char * strbuffer; /* mutable buffer for string (modified in calls to strtok()) */
 	double ans; /* answer to return */
-	token_queue queue_infix, queue_postfix;
+	struct token_queue queue_infix, queue_postfix;
 
 	/* copy str into mutable buffer */
 	strbuffer = strcpy((char *)malloc(strlen(str)+1),str);
@@ -104,8 +211,8 @@ double evaluate(const char * str) {
 }
 
 /* constructs a queue of tokens in infix order from a space-delimited string */
-token_queue expr_to_infix(char * str) {
-	token_queue queue_infix; /* queue with infix ordering */
+struct token_queue expr_to_infix(char * str) {
+	struct token_queue queue_infix; /* queue with infix ordering */
 	enum token_type type = OPERATOR;
 	union token_value value;
 
@@ -168,16 +275,16 @@ token_queue expr_to_infix(char * str) {
 }
 
 /* creates a queue of tokens in postfix order from a queue of tokens in infix order */
-/* postcondition: returned queue contains all the tokens, and pqueue_infix should be
+/* postcondition: returned queue contains all the tokens, and pqueue_infix should be 
    empty */
-token_queue infix_to_postfix(token_queue * pqueue_infix) {
+struct token_queue infix_to_postfix(struct token_queue * pqueue_infix) {
 	/* TODO: construct postfix-ordered queue from infix-ordered queue;
 	   all tokens from infix queue should be added to postfix queue or freed */
 }
 
 /* evalutes the postfix expression stored in the queue */
 /* postcondition: returned value is final answer, and pqueue_postfix should be empty */
-double evaluate_postfix(token_queue * pqueue_postfix) {
+double evaluate_postfix(struct token_queue * pqueue_postfix) {
 	/* TODO: process postfix-ordered queue and return final answer;
 	   all tokens from postfix-ordered queue is freed */
 }
