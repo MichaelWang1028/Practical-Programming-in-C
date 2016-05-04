4654e5a4f8ca3de96e7ccf3f40bdc199923cae4f
M <lmiguelvargasf@gmail.com>
2016-05-04 10:20:16 -0500

    Remove #define pre-processor directives

--- pset6a/prob1.c
+++ pset6a/prob1.c
@@ -11,13 +11,6 @@
 #include <string.h>
 #include "prob1.h"
 
-/* maximum length of input string (including newline character) */
-#define INPUT_MAX 2048
-
-/* enable (1) or disable (0) parentheses checking in parsing strings */
-/* leave disabled for part (a); enable for part (b) */
-#define PARSE_PARENS 0
-
 /* type of token */
 enum token_type {
 	OPERAND, /* number */
--- pset6a/prob1.h
+++ pset6a/prob1.h
@@ -1,6 +1,11 @@
 #ifndef PROB1_H
 #define PROB1_H
 
+/* maximum length of input string (including newline character) */
+#define INPUT_MAX 2048
 
+/* enable (1) or disable (0) parentheses checking in parsing strings */
+/* leave disabled for part (a); enable for part (b) */
+#define PARSE_PARENS 0
 
 #endif
