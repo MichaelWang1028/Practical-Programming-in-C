8d76806796de9d928894b49b4c958f151ccb461c
M <lmiguelvargasf@gmail.com>
2016-05-04 11:21:49 -0500

    Remove functions and function prototypes that now are in evaluation.c and evaluation.h

--- pset6a/prob1.c
+++ pset6a/prob1.c
@@ -55,7 +55,7 @@ void push(p_expr_token * ptop, const p_expr_token ptoken) {
 	*ptop = ptoken;
 }
 
-/* pop (remove) token from top of stack 
+/* pop (remove) token from top of stack
    input: pointer to top token pointer of stack
    output: top token pointer (or NULL, if stack was empty)
    postcondition: ptop points to next token in stack */
@@ -69,7 +69,7 @@ p_expr_token pop(p_expr_token * ptop) {
 }
 
 /* allocate new token on heap, with specified type and value; the token is initially
-   un-linked (field "linked_token" == NULL) 
+   un-linked (field "linked_token" == NULL)
    note: token must be freed using free() after use */
 p_expr_token new_token(const enum token_type type, const union token_value value) {
 	p_expr_token ptoken = (p_expr_token)malloc(sizeof(struct s_expr_token));
@@ -77,106 +77,4 @@ p_expr_token new_token(const enum token_type type, const union token_value value
 	ptoken->value = value;
 	ptoken->linked_token = NULL;
 	return ptoken;
-}
-
-/* handles evaluation process (calls above functions) for expression string str */
-/* returns the final answer */
-double evaluate(const char * str) {
-	char * strbuffer; /* mutable buffer for string (modified in calls to strtok()) */
-	double ans; /* answer to return */
-	struct token_queue queue_infix, queue_postfix;
-
-	/* copy str into mutable buffer */
-	strbuffer = strcpy((char *)malloc(strlen(str)+1),str);
-
-	/* get queue of tokens in infix order from string buffer */
-	queue_infix = expr_to_infix(strbuffer);
-
-	/* get queue of tokens in postfix order from infix-ordered queue */
-	queue_postfix = infix_to_postfix(&queue_infix);
-
-	/* get answer from postfix-ordered queue */
-	ans = evaluate_postfix(&queue_postfix);
-
-	free(strbuffer); /* free memory from heap */
-	return ans;
-}
-
-/* constructs a queue of tokens in infix order from a space-delimited string */
-struct token_queue expr_to_infix(char * str) {
-	struct token_queue queue_infix; /* queue with infix ordering */
-	enum token_type type = OPERATOR;
-	union token_value value;
-
-	/* initialize the queue to empty */
-	queue_infix.front = NULL;
-	queue_infix.back = NULL;
-
-	/* delimiter string for strtok() -- contains whitespace characters */
-#define DELIMS_STR " \n\r\t"
-	for (str = strtok(str, DELIMS_STR); str; str = strtok(NULL, DELIMS_STR)) {
-		/* parse token */
-		if (strlen(str) == 1) { /* operators are all 1 character */
-			switch (str[0]) {
-			case '+':
-				type = OPERATOR;
-				value.op_code = ADD;
-				break;
-			case '-':
-				/* check previous token to distinguish between
-				   negate (unary) and subtract (binary) */
-				if (type == OPERATOR)
-					value.op_code = NEGATE; /* unary */
-#if PARSE_PARENS
-				else if (type == LPARENS)
-					value.op_code = NEGATE; /* unary */
-#endif
-				else
-					value.op_code = SUBTRACT; /* binary */
-				type = OPERATOR;
-				break;
-			case '*':
-				type = OPERATOR;
-				value.op_code = MULTIPLY;
-				break;
-			case '/':
-				type = OPERATOR;
-				value.op_code = DIVIDE;
-				break;
-#if PARSE_PARENS
-			case '(':
-				type = LPARENS;
-				break;
-			case ')':
-				type = RPARENS;
-				break;
-#endif
-			default:
-				/* not an operator */
-				type = OPERAND;
-				value.operand = strtod(str, NULL);
-			}
-		} else {
-			type = OPERAND;
-			value.operand = strtod(str, NULL);
-		}
-		/* add token with parsed type and value to end of queue */
-		enqueue(&queue_infix, new_token(type, value));
-	}
-	return queue_infix;
-}
-
-/* creates a queue of tokens in postfix order from a queue of tokens in infix order */
-/* postcondition: returned queue contains all the tokens, and pqueue_infix should be 
-   empty */
-struct token_queue infix_to_postfix(struct token_queue * pqueue_infix) {
-	/* TODO: construct postfix-ordered queue from infix-ordered queue;
-	   all tokens from infix queue should be added to postfix queue or freed */
-}
-
-/* evalutes the postfix expression stored in the queue */
-/* postcondition: returned value is final answer, and pqueue_postfix should be empty */
-double evaluate_postfix(struct token_queue * pqueue_postfix) {
-	/* TODO: process postfix-ordered queue and return final answer;
-	   all tokens from postfix-ordered queue is freed */
 }
--- pset6a/prob1.h
+++ pset6a/prob1.h
@@ -8,6 +8,8 @@
 /* leave disabled for part (a); enable for part (b) */
 #define PARSE_PARENS 0
 
+extern char operators[];
+
 /* type of token */
 enum token_type {
 	OPERAND, /* number */
@@ -72,20 +74,5 @@ p_expr_token pop(p_expr_token * ptop);
 
 /* creates a new token in dynamic memory (using malloc()) */
 p_expr_token new_token(const enum token_type type, const union token_value value);
-
-/* constructs a queue of tokens in infix order from a space-delimited string */
-struct token_queue expr_to_infix(char * str);
-
-/* creates a queue of tokens in postfix order from a queue of tokens in infix order */
-/* postcondition: returned queue contains all the tokens, and pqueue_infix should be
-   empty */
-struct token_queue infix_to_postfix(struct token_queue * pqueue_infix);
-
-/* evalutes the postfix expression stored in the queue */
-/* postcondition: returned value is final answer, and pqueue_postfix should be empty */
-double evaluate_postfix(struct token_queue * pqueue_postfix);
-
-/* handles evaluation process (calls above functions) for expression string str */
-double evaluate(const char * str);
 
 #endif
