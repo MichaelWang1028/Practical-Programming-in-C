51da69dc1a207893b2718a44a26e8fa8b78d1919
M <lmiguelvargasf@gmail.com>
2016-05-05 14:16:05 -0500

    Move trie struct definition from prob2.h to trie.h

--- pset6a/trie.h
+++ pset6a/trie.h
@@ -1,6 +1,13 @@
 #ifndef TRIE_H
 #define TRIE_H
 
+/* the trie node data structure */
+struct s_trie_node
+{
+	char * translation; /* NULL if node not a word */
 
+	/* pointer array to child nodes */
+	struct s_trie_node * children[UCHAR_MAX + 1];
+};
 
 #endif
