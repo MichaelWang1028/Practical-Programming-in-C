6082c122162bc23bffae6b0a1652141aa9191f92
M <lmiguelvargasf@gmail.com>
2016-05-05 15:18:28 -0500

    Free non-null translations

--- pset6a/prob2/trie.c
+++ pset6a/prob2/trie.c
@@ -17,18 +17,19 @@ trie_node * new_node(void) {
 	return p_node;
 }
 
-/* delete node and all its children
+/* using recursion delete node and all its children
+	 it also deletes all non-null translations.
    input: pointer to node to delete
    postcondition: node and children are freed */
 void delete_node(trie_node * pnode) {
-	/* TODO: delete node and all its children
-	   Be sure to free non-null translations!
-	   Hint: use recursion
-	 */
 	 for (int i = 0; i < UCHAR_MAX + 1; i++) {
 		 if (pnode->children[i] != NULL) {
 			 delete_node(pnode->children[i]);
 		 }
+	 }
+
+	 if (pnode->translation != NULL) {
+		 free(pnode->translation);
 	 }
 	 free(pnode);
 }
