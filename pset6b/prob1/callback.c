ed84f6de5d3902715d1f9d6f4290e8acfe257b13
M <lmiguelvargasf@gmail.com>
2016-05-14 13:16:18 -0500

    Create main.c file and separate code

--- pset6b/prob1/callback.c
+++ pset6b/prob1/callback.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include "callback.h"
 
-student class[]={
+student class[10] = {
   "Sean","Penn",2,21,
   "Sean","Connery",4,25,
   "Angelina","Jolie",3,22,
@@ -72,33 +72,4 @@ void printrec(void* prec,void* arg)
 void isolder(void* prec,void* arg)
 {
 
-}
-
-int main()
-{
-  int nstudents=sizeof(class)/sizeof(student);
-  int age;
-
-  puts("Raw records:");
-  puts("-------------------------------");
-  apply(class,nstudents,printrec,NULL);
-
-  /*sort based on first name*/
-  puts("Sorted by first name:");
-  puts("-------------------------------");
-  qsort(class,nstudents,sizeof(student),compare_first_name);
-  apply(class,nstudents,printrec,NULL);
-
-  /*sort based on last name*/
-  puts("Sorted by last name:");
-  puts("-------------------------------");
-  qsort(class,nstudents,sizeof(student),compare_last_name);
-  apply(class,nstudents,printrec,NULL);
-
-  /*print people older than 20*/
-  puts("People older than 20:");
-  puts("-------------------------------");
-  age=20;
-  apply(class,nstudents,isolder,&age);
-  return 0;
 }
--- pset6b/prob1/callback.h
+++ pset6b/prob1/callback.h
@@ -9,4 +9,39 @@ typedef struct
   int  age;
 } student;
 
+extern student class[10];
+
+/*
+  TODO
+  @function compare_first_name
+  @desc     compares first name of two records.
+*/
+int compare_first_name(const void* a,const void* b);
+
+/*
+  TODO
+  @function compare_last_name
+  @desc     compares last name of two records.
+*/
+int compare_last_name(const void* a,const void* b);
+
+/*!
+  @function apply
+  @desc     applies
+ */
+void apply(student* sarr,int nrec,void (*fp)(void* prec,void* arg),void* arg);
+
+/*
+  @function printrec
+  @desc     prints student record
+*/
+void printrec(void* prec,void* arg);
+
+/*
+  @function isolder
+  @desc     prints student record onlyl if the student is older than *((int*)arg)
+  NOTE: use the same format as
+*/
+void isolder(void* prec,void* arg);
+
 #endif
