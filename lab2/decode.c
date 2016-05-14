035cd1151b12183c7e7296697d334f54764a6bf5
M <lmiguelvargasf@gmail.com>
2016-05-14 18:45:57 -0500

    Move struct definitions from c to h file

--- lab2/decode.c
+++ lab2/decode.c
@@ -3,20 +3,6 @@
 #include <string.h>
 #include "decode.h"
 
-typedef struct tree_node
-{
-    struct  tree_node* left; /*used when in tree*/
-    struct  tree_node*right; /*used when in tree*/
-    int     isleaf;
-    char     symbol;
-} tree_node;
-
-typedef struct code
-{
-	int		symbol;
-	char	str_code[MAX_LEN];
-} code;
-
 /*global variables*/
 tree_node* root = NULL; /*tree of symbols*/
 
--- lab2/decode.h
+++ lab2/decode.h
@@ -4,4 +4,18 @@
 #define MAX_SYMBOLS 255
 #define MAX_LEN     10
 
+typedef struct tree_node
+{
+    struct  tree_node* left; /*used when in tree*/
+    struct  tree_node*right; /*used when in tree*/
+    int     isleaf;
+    char     symbol;
+} tree_node;
+
+typedef struct code
+{
+	int		symbol;
+	char	str_code[MAX_LEN];
+} code;
+
 #endif
