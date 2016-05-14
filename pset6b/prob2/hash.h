bb11580efdbe736398629195da048ffda7325d4d
M <lmiguelvargasf@gmail.com>
2016-05-14 16:47:29 -0500

    Apply beautify

--- pset6b/prob2/hash.h
+++ pset6b/prob2/hash.h
@@ -7,35 +7,35 @@
 
 typedef struct wordrec
 {
-  char* word;
-  unsigned long count;
-  struct wordrec* next;
+        char* word;
+        unsigned long count;
+        struct wordrec* next;
 } wordrec;
 
 /*
-  @function walloc
-  @desc     produces a new node
-*/
+   @function walloc
+   @desc     produces a new node
+ */
 wordrec* walloc(const char* str);
 
 /*
-  @function hashstring
-  @desc     produces hash code for a string
+   @function hashstring
+   @desc     produces hash code for a string
             multipliers 31,35 have been found to work well
-*/
+ */
 unsigned long hashstring(const char* str);
 
 /*
-  @function lookup
-  @desc     returns a pointer to the word or creates
+   @function lookup
+   @desc     returns a pointer to the word or creates
             it if required
-*/
+ */
 wordrec* lookup(const char* str, int create);
 
 /*
-  @function cleartable()
-  @desc     reclaims memory
-*/
+   @function cleartable()
+   @desc     reclaims memory
+ */
 void cleartable();
 
 #endif
