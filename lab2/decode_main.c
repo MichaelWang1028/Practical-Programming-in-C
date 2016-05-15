41c672e0755981239e117db6143a8e7d299aafa3
M <lmiguelvargasf@gmail.com>
2016-05-14 19:19:39 -0500

    Remove redundant comments

--- lab2/decode_main.c
+++ lab2/decode_main.c
@@ -5,20 +5,20 @@ int main()
 {
 	FILE* output_file;
 	FILE* input_file;
-	/*allocate root*/
+
 	root = allocate_new_tree_node();
 	input_file = fopen(CODE_FILE, "r");
-	/*build tree*/
+
 	build_tree(input_file);
 	fclose(input_file);
 
-	/*decode*/
+
 	input_file = fopen(IN_FILE, "r");
 	output_file = fopen(OUT_FILE, "w");
 	decode(input_file, output_file);
 	fclose(input_file);
 	fclose(output_file);
-	/*cleanup*/
+
 	free_tree(root);
 	getchar();
 	return 0;
