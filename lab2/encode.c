fa5b1a6533998f84a84020b700d94830c26e2e3d
M <lmiguelvargasf@gmail.com>
2016-05-15 12:48:12 -0500

    Fix format

--- lab2/encode.c
+++ lab2/encode.c
@@ -10,32 +10,33 @@ struct cnode* chead = NULL;/*list of code*/
 
 tree_node* allocate_new_node(int symbol, float freq)
 {
-    tree_node* p = (tree_node *) malloc(sizeof(tree_node));
-    if (p != NULL)
-    {
-        p->left = p->right = p->parent = p->next = NULL;
-        p->symbol = symbol;
-        p->freq = freq;
-		    p->isleaf = 1;
-    }
-    return p;
+  tree_node* p = (tree_node *) malloc(sizeof(tree_node));
+
+  if (p != NULL)
+  {
+    p->left = p->right = p->parent = p->next = NULL;
+    p->symbol = symbol;
+    p->freq = freq;
+    p->isleaf = 1;
+  }
+
+  return p;
 }
 
 void display_tree_node_list(tree_node* head)
 {
-    tree_node* p = NULL;
-    printf("list:");
+  tree_node* p = NULL;
+  printf("list:");
 
-    for(p = head; p != NULL; p = p->next)
-    {
-        printf("(%c,%f) ", p->symbol, p->freq);
-    }
+  for(p = head; p != NULL; p = p->next) {
+    printf("(%c,%f) ", p->symbol, p->freq);
+  }
 
-    printf("\n");
+  printf("\n");
 }
 
 /*
-  NOTE:     makes use of global variable qhead
+  NOTE: makes use of global variable qhead
 */
 void insert_into_priority_queue(tree_node* p)
 {
@@ -43,25 +44,21 @@ void insert_into_priority_queue(tree_node* p)
   tree_node* prev = NULL;
   printf("inserting:%c,%f\n", p->symbol, p->freq);
 
-  if (qhead == NULL) /*qhead is null*/
-  {
+  if (qhead == NULL) {/*qhead is null*/
     /*TODO: write code to insert when queue is empty*/
   }
   /*TODO: write code to find correct position to insert*/
-  if (curr == qhead)
-  {
+  if (curr == qhead) {
     /*TODO: write code to insert before the current start*/
-  }
-  else /*insert between prev and next*/
-  {
+  } else { /*insert between prev and next*/
     /*TODO: write code to insert in between*/
   }
 }
 
 /*
-    @function pop_priority_queue
-    @desc     removes the first element
-    NOTE:     makes use of global variable qhead
+  @function pop_priority_queue
+  @desc     removes the first element
+  NOTE:     makes use of global variable qhead
 */
 tree_node* pop_priority_queue()
 {
@@ -89,7 +86,7 @@ void generate_code(tree_node* root, int depth)
 			TODO: follow parent pointer to the top
 			to generate the code string
 		*/
-		printf("built code:%c,%s\n",symbol,code[symbol]);
+		printf("built code:%c,%s\n", symbol, code[symbol]);
 	} else {
 		generate_code(root->left, depth + 1);
 		generate_code(root->right, depth + 1);
@@ -102,8 +99,7 @@ void generate_code(tree_node* root, int depth)
 */
 void dump_code(FILE* fp)
 {
-	for(int i = 0; i < MAX_SYMBOLS; i++)
-	{
+	for(int i = 0; i < MAX_SYMBOLS; i++) {
 		if (code[i][0]) /*non empty*/
 			fprintf(fp,"%c %s\n", i, code[i]);
 	}
@@ -115,8 +111,7 @@ void dump_code(FILE* fp)
 */
 void encode(char* str, FILE* fout)
 {
-	while (*str)
-	{
+  while (*str) {
 		fprintf(fout, "%s", code[*str]);
 		str++;
 	}
@@ -129,7 +124,7 @@ int main()
 {
   /*test priority queue*/
   tree_node* p = NULL;
-  tree_node* lc,*rc;
+  tree_node* lc, *rc;
   float freq[] = {0.01, 0.04, 0.05, 0.11, 0.19, 0.20, 0.4};
 	int NCHAR = 7; /*number of characters*/
   int i = 0;
@@ -140,58 +135,55 @@ int main()
 	memset(code, 0, sizeof(code));
 
 	/*testing queue*/
-    insert_into_priority_queue(allocate_new_node('a',0.1));
-    insert_into_priority_queue(allocate_new_node('b',0.2));
-    insert_into_priority_queue(allocate_new_node('c',0.15));
-    /*making sure it pops in the right order*/
+  insert_into_priority_queue(allocate_new_node('a', 0.1));
+  insert_into_priority_queue(allocate_new_node('b', 0.2));
+  insert_into_priority_queue(allocate_new_node('c', 0.15));
+  /*making sure it pops in the right order*/
 	puts("making sure it pops in the right order");
-	while((p=pop_priority_queue()))
-    {
-        free(p);
-    }
+	while ((p = pop_priority_queue())) {
+    free(p);
+  }
 
+	qhead = NULL;
+  /*initialize with freq*/
+  for(i = 0; i < NCHAR; i++) {
+    insert_into_priority_queue(allocate_new_node('a'+i, freq[i]));
+  }
 
-
-	qhead=NULL;
-    /*initialize with freq*/
-    for(i=0;i<NCHAR;i++)
-    {
-        insert_into_priority_queue(allocate_new_node('a'+i,freq[i]));
-    }
-    /*build tree*/
-    for(i=0;i<NCHAR-1;i++)
-    {
-        lc=pop_priority_queue();
-        rc=pop_priority_queue();
-        /*create parent*/
-        p=allocate_new_node(0,lc->freq+rc->freq);
-        /*set parent link*/
-        lc->parent=rc->parent=p;
-        /*set child link*/
-        p->right=rc; p->left=lc;
+  /*build tree*/
+  for(i = 0; i < NCHAR - 1; i++) {
+    lc = pop_priority_queue();
+    rc = pop_priority_queue();
+    /*create parent*/
+    p = allocate_new_node(0,lc->freq+rc->freq);
+    /*set parent link*/
+    lc->parent = rc->parent = p;
+    /*set child link*/
+    p->right = rc; p->left = lc;
 		/*make it non-leaf*/
-		p->isleaf=0;
-        /*add the new node to the queue*/
-        insert_into_priority_queue(p);
-    }
-    /*get root*/
-    root=pop_priority_queue();
+		p->isleaf = 0;
+    /*add the new node to the queue*/
+    insert_into_priority_queue(p);
+  }
+
+  /*get root*/
+  root = pop_priority_queue();
 	/*build code*/
-	generate_code(root,0);
+	generate_code(root, 0);
 	/*output code*/
 	puts("code:");
-	fout=fopen(CODE_FILE,"w");
+	fout = fopen(CODE_FILE, "w");
 	dump_code(stdout);
 	dump_code(fout);
 	fclose(fout);
 
 	/*encode a sample string*/
 	puts("orginal:abba cafe bad");
-	fout=fopen(OUT_FILE,"w");
-	encode("abba cafe bad",stdout);
-	encode("abba cafe bad",fout);
+	fout = fopen(OUT_FILE,"w");
+	encode("abba cafe bad", stdout);
+	encode("abba cafe bad", fout);
 	fclose(fout);
 	getchar();
 	/*TODO: clear resources*/
-    return 0;
+  return 0;
 }
