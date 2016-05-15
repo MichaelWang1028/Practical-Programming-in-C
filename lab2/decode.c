bd4ec4ef97709284d5af9bbcc2a68b687fd1c97d
M <lmiguelvargasf@gmail.com>
2016-05-14 19:18:53 -0500

    Add decode_main.c file and move main() function to it

--- lab2/decode.c
+++ lab2/decode.c
@@ -76,26 +76,3 @@ void free_tree(tree_node* root)
 	free_tree(root->left);
 	free(root);
 }
-
-int main()
-{
-	FILE* output_file;
-	FILE* input_file;
-	/*allocate root*/
-	root = allocate_new_tree_node();
-	input_file = fopen(CODE_FILE, "r");
-	/*build tree*/
-	build_tree(input_file);
-	fclose(input_file);
-
-	/*decode*/
-	input_file = fopen(IN_FILE, "r");
-	output_file = fopen(OUT_FILE, "w");
-	decode(input_file, output_file);
-	fclose(input_file);
-	fclose(output_file);
-	/*cleanup*/
-	free_tree(root);
-	getchar();
-	return 0;
-}
