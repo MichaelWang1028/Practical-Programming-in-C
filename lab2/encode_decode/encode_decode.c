84853052b9733e17cdd443120677d7ad980f3bf4
M <lmiguelvargasf@gmail.com>
2016-05-15 20:56:13 -0500

    Move function

--- lab2/encode_decode/encode_decode.c
+++ lab2/encode_decode/encode_decode.c
@@ -1,1 +1,18 @@
 #include "encode_decode.h"
+
+void get_frequency_from_file(FILE* file_name)
+{
+  char c;
+  unsigned long long total = 0;
+
+  while ((c = getc(file_name)) != EOF) {
+    frequencies[(unsigned char) c]++;
+    total++;
+  }
+
+  for (int i = 0; i < MAX_SYMBOLS; i++) {
+    if (frequencies[i] > 0) {
+      frequencies[i] /= total;
+    }
+  }
+}
--- lab2/encode_decode/encode_decode.h
+++ lab2/encode_decode/encode_decode.h
@@ -1,6 +1,14 @@
 #ifndef ENCODE_DECODE_H
 #define ENCODE_DECODE_H
 
+#include <stdio.h>
+#include "../encode/encode.h"
 
+#define READ_MODE "r"
+#define INPUT_FILE "book.txt"
+
+extern float frequencies[MAX_SYMBOLS];
+
+void get_frequency_from_file(FILE* file_name);
 
 #endif
