3a342454799541785463c8648af12aba7fffdafe
M <lmiguelvargasf@gmail.com>
2016-05-15 19:50:21 -0500

    Modify MAX_LEN

--- lab2/encode_decode/encode_decode.h
+++ lab2/encode_decode/encode_decode.h
@@ -1,6 +1,10 @@
 #ifndef ENCODE_DECODE_H
 #define ENCODE_DECODE_H
 
+#ifdef MAX_LEN
+#undef MAX_LEN
+#endif
 
+#define MAX_LEN 255
 
 #endif
--- lab2/encode_decode/main.c
+++ lab2/encode_decode/main.c
@@ -1,6 +1,9 @@
+#include <stdio.h>
+#include "../encode/encode.h"
 #include "encode_decode.h"
 
 int main()
 {
+  printf("%d\n", MAX_LEN);
   return 0;
 }
