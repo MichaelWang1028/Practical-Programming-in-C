62b7f9a292ae8fe546fc173292870d9d12fffb2d
M <lmiguelvargasf@gmail.com>
2016-05-23 11:05:10 -0500

    Fix format

--- pset5/linked_list/linked_list.h
+++ pset5/linked_list/linked_list.h
@@ -4,15 +4,15 @@
 typedef struct node
 {
   int data;
-  struct node* next;
+  struct node * next;
 } node;
 
-node* create_node(int data);
-node* add_node_at_front(node* head, int data);
-node* add_node(node* head, int data);
-node* find_node(node* head, int data);
-node* delete_node(node* head, node* pointing_element);
-void free_list(node* head);
-void display(node* head);
+node * create_node(int data);
+node * add_node_at_front(node * head, int data);
+node * add_node(node * head, int data);
+node * find_node(node * head, int data);
+node * delete_node(node * head, node * pointing_element);
+void free_list(node * head);
+void display(node * head);
 
 #endif
