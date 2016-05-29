b24fbd043e2c7915d9c49040ebe95d7c5066fb42
M <lmiguelvargasf@gmail.com>
2016-05-29 14:34:14 -0500

    Test implemented functions

--- pset4/string_tokens/test_string_tokens.c
+++ pset4/string_tokens/test_string_tokens.c
@@ -1,6 +1,22 @@
+#include <stdio.h>
 #include "string_tokens.h"
 
 int main(void)
 {
-  return 0;
+  char str[80] = "This is - www.tutorialspoint.com - website. A really good nice, clear page.";
+   const char * delims  = "- .,";
+   char *token;
+
+   /* get the first token */
+   token = strtok(str, delims);
+
+   /* walk through other tokens */
+   while( token != NULL )
+   {
+      printf("%s\n", token );
+
+      token = strtok(NULL, delims);
+   }
+
+   return(0);
 }
