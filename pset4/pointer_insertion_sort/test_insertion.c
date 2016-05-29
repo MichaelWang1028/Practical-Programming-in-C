20eabfa9795ffcdf3a9b49427e72d82b06efe6c0
M <lmiguelvargasf@gmail.com>
2016-05-29 09:44:27 -0500

    Rename file

--- pset4/pointer_insertion_sort/test_insertion.c
+++ pset4/pointer_insertion_sort/test_insertion.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include "shell_sort.h"
-#include "util.h"
+#include "insertion.h"
+#include "../util/util.h"
 
 int main()
 {
   printf("Before sorting:\n");
   print_array(array, array_size(array));
 
-  shellsort();
+  insertion_sort();
 
   printf("After sorting:\n");
   print_array(array, array_size(array));
